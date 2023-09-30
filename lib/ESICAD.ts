b9e05d35e93b659ad8c4d6f1fe8663e9e413ee99
Aurélien VERNAY <aurevernay@yahoo.fr>
2023-09-30 18:12:16 +0200

    edited gitignore added TSdoc

--- lib/ESICAD.ts
+++ lib/ESICAD.ts
@@ -3,16 +3,40 @@ const prompt = promptSync({
   sigint: true,
   eot: true,
 });
+
+/**
+ * Affiche un texte à l'écran
+ * @param {string} texte le texte que l'on veut afficher
+ */
 const afficher = (texte: string) => console.log(texte);
 
-function saisirTexte(message?: string): string {
+/**
+ * Demande à l'utilisateur de saisir une valeur qui sera interprétée
+ * comme une chaine de caractères.
+ * @param {string} message un message que l'on veut afficher avant de saisir (optionnel)
+ */
+const saisirTexte = (message?: string): string => {
   return prompt(message + "");
-}
-function saisirNombre(message?: string): number | null {
+};
+
+/**
+ * Demande à l'utilisateur de saisir une valeur qui sera interprétée
+ * comme une valeur numérique. Si l'entrée de l'utilisateur ne peut être traitée
+ *  comme un nombre, la valeur `null` sera retournée
+ * @param {string} message un message que l'on veut afficher avant de saisir (optionnel)
+ */
+const saisirNombre = (message?: string): number | null => {
   const output = Number(prompt(message + ""));
   return isNaN(output) ? null : output;
-}
-function saisirBooleen(message?: string): boolean {
+};
+
+/**
+ * Demande à l'utilisateur de saisir une valeur qui sera interprétée
+ * comme une valeur booléenne. La fonction comprendra comme vraie les entrées
+ * `vrai`,`VRAI`,`V`,`v`,`oui`,`OUI`,`o`,`O`,`true`,`TRUE`, et faux n'importe quelle autre valeur.
+ * @param {string} message un message que l'on veut afficher avant de saisir (optionnel)
+ */
+const saisirBooleen = (message?: string): boolean => {
   const output = prompt(message + "");
   let val = false;
   switch (output) {
@@ -22,6 +46,10 @@ function saisirBooleen(message?: string): boolean {
     case "v":
     case "oui":
     case "OUI":
+    case "yes":
+    case "YES":
+    case "y":
+    case "Y":
     case "o":
     case "O":
     case "true":
@@ -32,7 +60,8 @@ function saisirBooleen(message?: string): boolean {
       break;
   }
   return val;
-}
+};
+
 const helpers = { saisirBooleen, saisirNombre, saisirTexte, afficher };
 
 export { helpers as ESICADHelpers };
