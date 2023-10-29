9927b99999eebdb8842cd62e9f4d7ab77e58e13e
Aurélien VERNAY <aurevernay@yahoo.fr>
2023-10-29 22:09:08 +0100

    small change to index

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ let maVariable;
 ESICADHelpers.afficher("Bonjour, je suis un programme à débugger !");
 maVariable = ESICADHelpers.saisirNombre("Saisir un nombre : ");
 if (maVariable > 100) {
-  ESICADHelpers.afficher("C'est plus grand que 100");
+  ESICADHelpers.afficher("C'est plus grand que 💯");
 } else if (maVariable < 100) {
-  ESICADHelpers.afficher("C'est moins grand que 100");
+  ESICADHelpers.afficher("C'est moins grand que 💯");
 } else {
-  ESICADHelpers.afficher("C'est 100");
+  ESICADHelpers.afficher("C'est 💯 😲");
 }
